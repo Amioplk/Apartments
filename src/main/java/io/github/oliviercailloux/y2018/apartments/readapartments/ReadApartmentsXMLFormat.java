3db38f212b0846663a06326c6b432358a1ce564b
Amioplk <amir.worms@dauphine.eu>
2019-05-28 12:56:38 +0200

    rm unused import Field

--- src/main/java/io/github/oliviercailloux/y2018/apartments/readapartments/ReadApartmentsXMLFormat.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/readapartments/ReadApartmentsXMLFormat.java
@@ -2,7 +2,6 @@ package io.github.oliviercailloux.y2018.apartments.readapartments;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
