eddc8e12b6521426a83b7c6f01e55f77493e6e18
Aichaaa <aissatousakho97@gmail.com>
2019-06-18 11:03:37 +0200

    modif dans printApp

--- src/main/java/io/github/oliviercailloux/y2018/apartments/gui/PrintApartementGUI.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/gui/PrintApartementGUI.java
@@ -5,13 +5,39 @@ import java.io.IOException;
 
 import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
 import io.github.oliviercailloux.y2018.apartments.readapartments.ReadApartmentsXMLFormat;
+import io.github.oliviercailloux.y2018.apartments.toxmlproperties.XMLProperties;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMException;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+//import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
+//import org.eclipse.swt.widgets.Display;
+//import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+//import org.eclipse.swt.widgets.Shell;
+//import java.awt.List;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMException;
+import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
+
 
 /**
  * @author AITALIBRAHAM & SAKHO
@@ -19,9 +45,9 @@ import org.slf4j.LoggerFactory;
  */
 public class PrintApartementGUI {
 	/**
-	 * This class aims to print an apartment to the users
+	 * This class aims to print apartments to the users
 	 */
-	private final static Logger LOGGER = LoggerFactory.getLogger(CreateApartmentGUI.class);
+	//private final static Logger LOGGER = LoggerFactory.getLogger(CreateApartmentGUI.class);
 
 	/**
 	 * @param filename : the file describing apartments should be given in
@@ -31,67 +57,7 @@ public class PrintApartementGUI {
 	 * @throws IllegalArgumentException
 	 */
 
-	private Apartment apar;
-	protected Display display = new Display();
-	protected Shell shell = new Shell(display);
 
-	public PrintApartementGUI() {
-		this.setApar(new Apartment(20.0, "20 rue des consé", "Test Apartment"));
-
-	}
-
-	/**
-	 * Create a printer of an apartment given in xml format
-	 * @param fileName
-	 * @throws IOException
-	 * @throws IllegalArgumentException
-	 */
-	public PrintApartementGUI(String fileName) throws IOException, IllegalArgumentException {
-		ReadApartmentsXMLFormat xmlReader = new ReadApartmentsXMLFormat();
-		FileInputStream fileinputstream = new FileInputStream(fileName);
-		this.setApar(xmlReader.readApartment(fileinputstream));
-
-		LOGGER.info("Apratement has been loaded ");
-
-	}
-
-	public void print() {
-		
-		LOGGER.info("Test Apartment has been created");
-		Label title = new Label(shell, SWT.CENTER);
-		Label adress = new Label(shell, SWT.CENTER);
-		Label florArea = new Label(shell, SWT.CENTER);
-		Label wifi = new Label(shell, SWT.CENTER);
-
-		title.setText(getApar().getTitle());
-		adress.setText("adress : " + this.getApar().getAddress());
-		florArea.setText("floor Area : " + this.getApar().getFloorArea() + " m2 ");
-		wifi.setText("wifi : " + this.getApar().getWifi() + "");
-
-		wifi.pack();
-		florArea.pack();
-		adress.pack();
-		title.pack();
-
-		florArea.setLocation(100, 60);
-		adress.setLocation(100, 90);
-		wifi.setLocation(100, 120);
-		title.setLocation(100, 20);
-
-		shell.setText("Apartments");
-		shell.setMinimumSize(1000, 500);
-		shell.setSize(1000, 1500);
-		shell.pack();
-		shell.open();
-
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch())
-				display.sleep();
-		}
-		title.dispose();
-		display.dispose();
-
-	}
 	
 	/**
 	 * @param args
@@ -101,19 +67,65 @@ public class PrintApartementGUI {
 	 */
 	public static void main(String args[]) throws IllegalArgumentException, IOException {
 
-//		Exemple : 
-		 PrintApartementGUI prtApp = new PrintApartementGUI("src/test/resources/io/github/oliviercailloux/y2018/apartments/gui/apartTest.xml");
-//		PrintApartementGUI prtApp = new PrintApartementGUI();
-		prtApp.print();
-		
-	}
+		Display display = new Display();
+	       Shell shell = new Shell(display);
+	       shell.setText("SWT List (o7planning.org)");
+	       shell.setSize(700, 1000);
+	 
+	       RowLayout layout = new RowLayout(SWT.VERTICAL);
+	       layout.spacing = 10;
+	       layout.marginHeight = 10;
+	       layout.marginWidth = 10;
+	 
+	       shell.setLayout(layout);
+	       
+	       
 
-	public Apartment getApar() {
-		return apar;
-	}
+	       
+	       
+	 
+	       // Create a List
+	       // (Allows selecte multiple lines and display vertical scroll bar.).
+	       final List list = new List(shell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+	       list.setLayoutData(new RowData(240, 100));
+	       
+	       
+	       final String [] titres= new String[100];
+	       
+	       for(int i= 0; i<=9; i++) {
+	    	   //titres[i]= "titre "+ i;
+	    	   //titres= apps[i].getTitle();
+	    	   list.add("titre "+titres);
+	       }
+	       
+	       
+	 
+	       Label label = new Label(shell, SWT.NONE);
+	       label.setLayoutData(new RowData(240, SWT.DEFAULT));
+	 
+	       list.addSelectionListener(new SelectionAdapter() {
+	 
+	           @Override
+	           public void widgetSelected(SelectionEvent event) {
+	               int[] selections = list.getSelectionIndices();
+	               String outText = "";
+	               for (int i = 0; i < selections.length; i++) {
+	                   outText += selections[i] + " ";
+	               }
+	               label.setText("You selected: " + outText);
+	           }
+	 
+	       });
+	 
+	       shell.open();
+	       while (!shell.isDisposed()) {
+	           if (!display.readAndDispatch())
+	               display.sleep();
+	       }
+	       display.dispose();
+	   }
+	
 
-	public void setApar(Apartment apar) {
-		this.apar = apar;
-	}
+	
 
 }
