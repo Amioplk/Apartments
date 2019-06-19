e1ffa0a65b23ea8254ad2f69e7b491c8421c26f9
Amioplk <amir.worms@dauphine.eu>
2019-06-19 18:26:22 +0200

    Add MultiPartialValueFunctionTest

---
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/MultiPartialValueFunction.java
@@ -0,0 +1,12 @@
+package io.github.oliviercailloux.y2018.apartments.valuefunction;
+
+import org.junit.Test;
+
+public class MultiPartialValueFunction {
+
+	@Test
+	public void acceptDifferentTypesOfPartialValueFunction() {
+		
+	}
+
+}
