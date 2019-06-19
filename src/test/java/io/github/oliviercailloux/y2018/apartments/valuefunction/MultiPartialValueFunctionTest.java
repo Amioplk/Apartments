cd834a0f0658a9cd062cb29e958511d3373e48b8
Amioplk <amir.worms@dauphine.eu>
2019-06-19 18:26:49 +0200

    Correct class name

--- src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/MultiPartialValueFunctionTest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/MultiPartialValueFunctionTest.java
@@ -2,7 +2,7 @@ package io.github.oliviercailloux.y2018.apartments.valuefunction;
 
 import org.junit.Test;
 
-public class MultiPartialValueFunction {
+public class MultiPartialValueFunctionTest {
 
 	@Test
 	public void acceptDifferentTypesOfPartialValueFunction() {
