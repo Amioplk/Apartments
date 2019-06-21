b3fe5b0e928507744338f62fb791a55e55f131ee
Amioplk <amir.worms@dauphine.eu>
2019-06-21 15:47:45 +0200

    add constructo, getters, setters LinearValueFunction

--- src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/LinearValueFunction.java
+++ src/main/java/io/github/oliviercailloux/y2018/apartments/valuefunction/LinearValueFunction.java
@@ -1,5 +1,7 @@
 package io.github.oliviercailloux.y2018.apartments.valuefunction;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +33,17 @@ public class LinearValueFunction implements PartialValueFunction<Double> {
 		LOGGER.info("The interval ["+min+","+max+"] "+" has been set with success in the LinearValueFunction class.");
 	}
 
+	/**
+	 * @param min represents the lowerEndBound of the desired interval, along with its subjectiveValue associated
+	 * @param max represents the upperEndBound of the desired interval, along with its subjectiveValue associated
+	 */
+	public LinearValueFunction (Map.Entry<Double,Double> min, Map.Entry<Double,Double> max) {
+		this(min.getKey(), max.getKey());
+		setMinSubjectiveValue(min.getValue());
+		setMaxSubjectiveValue(max.getValue());
+	}
+	
+	
 	@Override
 	public double getSubjectiveValue(Double objectiveData) throws IllegalArgumentException {
 		if (interval.isEmpty())
@@ -49,6 +62,22 @@ public class LinearValueFunction implements PartialValueFunction<Double> {
 	@Override
 	public Double apply(Double objectiveData) {
 		return getSubjectiveValue(objectiveData);
+	}
+
+	public double getMinSubjectiveValue() {
+		return minSubjectiveValue;
+	}
+
+	public void setMinSubjectiveValue(double minSubjectiveValue) {
+		this.minSubjectiveValue = minSubjectiveValue;
+	}
+
+	public double getMaxSubjectiveValue() {
+		return maxSubjectiveValue;
+	}
+
+	public void setMaxSubjectiveValue(double maxSubjectiveValue) {
+		this.maxSubjectiveValue = maxSubjectiveValue;
 	}
 
 }
