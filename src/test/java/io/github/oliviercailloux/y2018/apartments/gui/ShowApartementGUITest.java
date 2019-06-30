10f3a67bd650882b229183ae687602723884a3e7
yanis <farouk.alibraham@gmail.com>
2019-07-01 00:11:52 +0200

    travis correction

--- src/test/java/io/github/oliviercailloux/y2018/apartments/gui/ShowApartementGUITest.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/gui/ShowApartementGUITest.java
@@ -1,7 +1,7 @@
 package io.github.oliviercailloux.y2018.apartments.gui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
 
